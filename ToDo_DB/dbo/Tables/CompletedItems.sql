ae04b9d96838172e3fe34b3755c1317844dd8579
Marco Preciado <mac46005@gmail.com>
2022-02-09 15:23:30 -0600

    Added foreign keys to certain tables

--- ToDo_DB/dbo/Tables/CompletedItems.sql
+++ ToDo_DB/dbo/Tables/CompletedItems.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ToDoItem_ID] INT NOT NULL, 
     [DateTimeCompleted] DATETIME2 NOT NULL, 
-    [Category_ID] INT NOT NULL
+    [Category_ID] INT NOT NULL, 
+    CONSTRAINT [FK_CompletedItems_ToCategory] FOREIGN KEY ([Category_ID]) REFERENCES [Categories]([Id])
 )
--- ToDo_DB/dbo/Tables/ToDoItems.sql
+++ ToDo_DB/dbo/Tables/ToDoItems.sql
@@ -4,5 +4,6 @@
     [ToDo] NVARCHAR(150) NOT NULL, 
     [DueDate] DATETIME2 NOT NULL, 
     [IsComplete] BIT NOT NULL,
-    [Category_ID] INT NOT NULL
+    [Category_ID] INT NOT NULL, 
+    CONSTRAINT [FK_ToDoItems_ToCategories] FOREIGN KEY ([Category_ID]) REFERENCES [Categories]([Id])
 )
