d658c42d92fef74fc6e99cae79c102eeb6c126e7
Marco Preciado <mac46005@gmail.com>
2022-02-10 07:49:29 -0600

    Added Stored procedures

---
+++ ToDo_DB/dbo/Stored Procedures/spCompletedItem_UpdateCompletedItem.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spCompletedItem_UpdateCompletedItem]
+	@ID int,
+	@ToDoItem_ID int,
+	@DateTimeCompleted DATETIME2,
+	@Category_ID int
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE CompletedItems
+	SET 
+	ToDoItem_ID = @ToDoItem_ID,
+	DateTimeCompleted = @DateTimeCompleted,
+	Category_ID = @Category_ID
+
+	WHERE Id = @ID
+END
---
+++ ToDo_DB/dbo/Stored Procedures/spToDoItems_AddToDoItem.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spToDoItems_AddToDoItem]
+	@ToDo NVARCHAR(50),
+	@DueDate DATETIME2,
+	@IsCompleted BIT,
+	@Category_ID int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO ToDoItems(ToDo,DueDate,IsComplete,Category_ID)
+	VALUES(@ToDo,@DueDate,@IsCompleted,@Category_ID)
+END
