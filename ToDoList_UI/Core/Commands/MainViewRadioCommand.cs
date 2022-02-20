836ea78b9d3dfd21c7e9f64b6cdaa12178d749f1
Marco Preciado <mac46005@gmail.com>
2022-02-20 01:02:28 -0600

    Getting information. Now to get category information into ToDoMainVie

---
+++ ToDoList_UI/Core/Commands/MainViewRadioCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ToDoList_UI.Core.Commands
+{
+    internal class MainViewRadioCommand : ICommand
+    {
+        Action<object> _execute;
+        Func<bool, bool> _canExecute;
+
+
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public MainViewRadioCommand(Action<object> execute,Func<object,bool> canExecute = null)
+        {
+
+        }
+
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
